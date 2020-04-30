bc80faccbd503338df880cabcb7c088106b5e799
KrupaKalola <61192813+KrupaKalola@users.noreply.github.com>
2020-04-30 12:01:18 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import React, { Component, useState } from 'react'
+import User from '../User'
+import Nav from '../Navbar'
+// export default class App extends Component {
+//     state={
+//         loggedIn:false
+//     }
+//     clickLogin=()=>{
+//         this.setState({loggedIn:true})
+
+//     }
+//     clickLogout=()=>{
+//         this.setState({loggedIn:false})
+
+//     }
+
+//     render() {
+//         const loggedIn = this.state.loggedIn
+//         if(loggedIn){
+//             return <Nav submitForm={this.clickLogout}/>
+//         }
+//         else{
+//             return <User submitForm={this.clickLogin}/>
+//         }
+//     }
+// }
+
+function App() {
+    const [loggedIn, setLoggedIn] = useState('false');
+    const clickLogin = () => {
+        setLoggedIn('true')
+        debugger
+        localStorage.setItem('loggedIn', 'true')
+    }
+    const clickLogout = () => {
+        setLoggedIn('false')
+        localStorage.setItem('loggedIn', 'false')
+
+    }
+    
+    if (loggedIn =='true') {
+        return <Nav submitForm={clickLogout} />
+    }
+    else {
+        return <User submitForm={clickLogin} />
+    }
+}
+
+export default App;
